08f0af3e0b948b7e2a0f0c1f06766b9704528f5f
Donuts_n_Tiaras <61537524+donutsntiaras@users.noreply.github.com>
2021-01-28 15:39:02 +0530

    Add files via upload

--- Mario-Game/Mario-Game/main.js
+++ Mario-Game/Mario-Game/main.js
@@ -6,15 +6,11 @@ function preload() {
 
 function setup() {
 	canvas = createCanvas(1240,336);
+    canvas.parent('canvas');
+
 	instializeInSetup(mario);
 }
 
 function draw() {
 	game()
 }
-
-
-
-
-
-
